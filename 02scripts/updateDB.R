8398284dbdd3ffdb9238b6f10c5c12507aeafaf6
frank0434 <jian.liu@plantandfood.co.nz>
2020-10-29 15:59:32 +1300

    try to add primary key into db - not successful

--- 02scripts/updateDB.R
+++ 02scripts/updateDB.R
@@ -14,5 +14,8 @@ waitingForUpdate <- grep("update", ls(), value = TRUE)
 
 lapply(waitingForUpdate, function(x){
   dbWriteTable(con, name = x, value = get(x), overwrite = TRUE, row.names = FALSE)
+  # sql <- paste0('ALTER TABLE "', x, '" ADD CONSTRAINT "', x, '.N_rate" PRIMARY KEY ("N_rate");')
+  # dbExecute(con, sql)
+  
 })
 dbDisconnect(con)
