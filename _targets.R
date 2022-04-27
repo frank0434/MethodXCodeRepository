74f92bd4835a9f015aea2ec78f2ce2aafa6640c3
frank0434 <jian.liu@plantandfood.co.nz>
2022-04-28 11:31:25 +1200

    copy source Data over to target plan

--- _targets.R
+++ _targets.R
@@ -16,5 +16,45 @@ list(
              ),
   tar_target(df_irrigation, 
              read_excel(tempfile, sheet = "IrrigationDiary", skip = 4,.name_repair = "universal") %>% 
-               as.data.table())
+               as.data.table()
+             ),
+
+# clean up the date -------------------------------------------------------
+
+
+# retrieve climate data ---------------------------------------------------
+  tar_target(me,
+             cf_user(Sys.getenv("clifro_usr"),
+                    Sys.getenv("clifro_pass"))
+             ),
+  tar_target(cf_datatype(select_1 =     c(9, 3),
+                         select_2 =     c(1, 1),
+                         check_box = list(4, 1),
+                         combo_box =    c(NA, NA))
+             ),
+  tar_target(my.station,
+             cf_station(17603L)
+             ),
+  tar_target(start_date, 
+             df$Date[1]
+             ),
+  tar_target(cf.datalist, 
+             cf_query(user = me,
+                      datatype = my.dts,
+                      station = my.station,
+                      start_date = paste(startd, "00"),
+                      end_date = Sys.Date())
+             ),
+
+# clean up the cliflo data ------------------------------------------------
+  tar_target(PET, 
+             as.data.table(cf.datalist[[1]])[, Date := as.Date(`Date(local)`, 
+                                                               format = "%Y-%m-%d")
+                                             ][, .(Date, PET = `Amount(mm)`)]
+             ),
+  tar_target(Rain, 
+             as.data.table(cf.datalist[[2]])[, Date := as.Date(`Date(local)`, 
+                                                               format = "%Y-%m-%d", tz = "NZ")
+                                             ][, .(Date, Rain = `Amount(mm)`)]
+             )
 )
