9e685dd337e11fd89800746136ed70702c18aa87
frank0434 <jian.liu@plantandfood.co.nz>
2022-07-20 11:18:03 +1200

    save hist

--- Scripts/00credentials.R
+++ Scripts/00credentials.R
@@ -1,19 +1,17 @@
 
 # credentials to grant file access to Sharepoint --------------------------
-USERNAME=""
-PASSWORD=""
+USERNAME="cflfcl"
+PASSWORD="Havelock2015"
 
 # Credentials to access National climate database -------------------------
-clifro_usr=""
-clifro_pass=""
-
+clifro_usr="linc3423"
+clifro_pass="broadfield"
 
 # Credentials to interactive with Github ----------------------------------
 GITHUB_PAT=""
 
 # Credentials to interactive with PostgreSQL database ---------------------
-con = dbConnect(PostgreSQL(), 
-                host = "",
-                dbname  = "",
-                user = "",
-                password = "" )
+dbcreds=list(host = "database.powerplant.pfr.co.nz",
+             dbname  = "cflfcl_MPI_SVS",
+             user = "cflfcl_SVS",
+             password = "vVsYDCzw7PD4Phb1")
